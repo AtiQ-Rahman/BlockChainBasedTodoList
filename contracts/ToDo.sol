f2435808885dd76af0ab7092b53fb93aef8814a7
AtiQ-Rahman <bsse0908@iit.du.ac.bd>
2021-10-21 00:27:18 +0600

    Step 1

--- contracts/ToDo.sol
+++ contracts/ToDo.sol
@@ -1,7 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
+
 
 contract ToDo {
-  constructor() public {
+  
+
+  struct Task{
+    uint id;
+    uint date;
+    string content;
+    string author;
+    bool done;
+  }
+
+  Task[] tasks;
+  function createTask(string memory _content,string memory _author) public {
+    tasks.push(Task(tasks.length,block.timestamp,_content,_author,false));
+  
+  }
+
+  function getTask(uint id) public view returns(uint,uint,string memory,string memory,bool )
+  {
+    return (
+     id,
+     tasks[id].date,
+     tasks[id].content,
+     tasks[id].author,
+     tasks[id].done
+    );
+  }
+  function getTasks() public view returns( Task[] memory)
+  {
+    return tasks;
+   
   }
 }
