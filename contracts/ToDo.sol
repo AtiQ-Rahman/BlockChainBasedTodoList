25ab721ef6a420619eef264fec46be0bc5985595
AtiQ-Rahman <bsse0908@iit.du.ac.bd>
2021-10-21 08:09:48 +0600

    step 3

--- contracts/ToDo.sol
+++ contracts/ToDo.sol
@@ -13,14 +13,28 @@ contract ToDo {
     bool done;
   }
 
-  Task[] tasks;
+  //Task[] tasks;
+  uint lastTaskId;
+  uint[] taskIds;
+  mapping(uint => Task) tasks;
+  //one way communication
+  event TaskCreated(uint, uint, string, string, bool);
+  constructor()  {
+    lastTaskId = 0;
+  }
+
+
   function createTask(string memory _content,string memory _author) public {
-    tasks.push(Task(tasks.length,block.timestamp,_content,_author,false));
+    lastTaskId++;
+    tasks[lastTaskId]=Task(lastTaskId,block.timestamp,_content,_author,false);
+    emit TaskCreated(lastTaskId, block.timestamp, _content, _author, false);
+    taskIds.push(lastTaskId);
   
   }
 
-  function getTask(uint id) public view returns(uint,uint,string memory,string memory,bool )
+  function getTask(uint id)  taskExists(id) public view  returns(uint,uint,string memory,string memory,bool )
   {
+    
     return (
      id,
      tasks[id].date,
@@ -29,9 +43,17 @@ contract ToDo {
      tasks[id].done
     );
   }
-  function getTasks() public view returns( Task[] memory)
+  function getTaskIds() public view returns( uint[] memory)
   {
-    return tasks;
+    return taskIds;
    
   }
+  //by default internal
+  modifier taskExists(uint id) {
+      if(tasks[id].id == 0) {
+        revert();
+      }
+      _;
+    }
+
 }
